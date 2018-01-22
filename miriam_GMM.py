f8c762d3b8068195061bf4cd5759445a8821f7a6
kasaugust <35238971+kasaugust@users.noreply.github.com>
2018-01-22 22:19:10 +0100

    Add files via upload

--- miriam_GMM.py
+++ miriam_GMM.py
@@ -1,9 +1,4 @@
-#componentów 4-8
-#ramkowanie 10/25 ms
-#mfcc 13
-#delty! python_speech_features.base.delta()
 import scipy.io.wavfile as wav
-import fnmatch
 import os
 import pickle
 import numpy
@@ -11,42 +6,13 @@ from python_speech_features import mfcc
 from python_speech_features import base
 from sklearn.mixture import GaussianMixture as GMM
 
-recordings=8
-ramkowanie=0.025
-components=4
-delta_memory=2
-path = os.getcwd()
-allMFCC = numpy.empty([1, recordings], dtype=object)
-i=0
-for file in os.listdir(os.path.join(path, 'miriam')):
-    fs, samples = wav.read(os.path.join(path, 'miriam', file))
-    MFCC = mfcc(samples, fs,winlen=ramkowanie, nfft=round(ramkowanie * fs) + 1, numcep=13)
-    delta=base.delta(MFCC, delta_memory)
-    delta_delta=base.delta(delta, delta_memory)
-    MFCC_and_deltas=numpy.c_[MFCC,delta,delta_delta]
-    allMFCC[0, i] = MFCC_and_deltas
-    i = i + 1
-# w nazwie pliku jest liczba parametrow mfcc z ktorej zostala funkcja wywołana
-#with open('mfcc_miriam.bin', 'wb') as f:
-#    pickle.dump(allMFCC, f)
-#    f.close()
-
-model= GMM(components, covariance_type="diag")
-tmp_data = numpy.zeros((0, 39))
-for j in range(0, recordings):
-    tmp_data = numpy.r_[tmp_data, allMFCC[0, j]]
-model.fit(tmp_data)
-with open('model.bin', 'wb') as f:
-    pickle.dump(model, f)
-    f.close()
-def if_Miriam(ramkowanie, delta_memory):
+def if_Miriam(ramkowanie, delta_memory,path):
     treshold = -50
-    path="miriam/1.wav"
     with open('model.bin', 'rb') as f:
         model=pickle.load(f)
         f.close()
     fs, data = wav.read(path)
-    MFCC = mfcc(samples, fs,winlen=ramkowanie, nfft=round(ramkowanie * fs) + 1, numcep=13)
+    MFCC = mfcc(data, fs,winlen=ramkowanie, nfft=round(ramkowanie * fs) + 1, numcep=10)
     delta=base.delta(MFCC, delta_memory)
     delta_delta=base.delta(delta, delta_memory)
     MFCC_and_deltas = numpy.c_[MFCC, delta, delta_delta]
@@ -56,4 +22,29 @@ def if_Miriam(ramkowanie, delta_memory):
         return True
     else:
         return False
-print(if_Miriam(ramkowanie,delta_memory))
+recordings=32
+ramkowanie=0.025
+components=4
+delta_memory=2
+path = os.getcwd()
+allMFCC = numpy.empty([1, recordings], dtype=object)
+i=0
+for file in os.listdir(os.path.join(path, 'miriam')):
+    fs, samples = wav.read(os.path.join(path, 'miriam', file))
+    MFCC = mfcc(samples, fs,winlen=ramkowanie, nfft=round(ramkowanie * fs) + 1, numcep=10)
+    delta=base.delta(MFCC, delta_memory)
+    delta_delta=base.delta(delta, delta_memory)
+    MFCC_and_deltas=numpy.c_[MFCC,delta,delta_delta]
+    allMFCC[0, i] = MFCC_and_deltas
+    i = i + 1
+
+model= GMM(components, covariance_type="diag")
+tmp_data = numpy.zeros((0, 30))
+for j in range(0, recordings):
+    tmp_data = numpy.r_[tmp_data, allMFCC[0, j]]
+model.fit(tmp_data)
+with open('model.bin', 'wb') as f:
+    pickle.dump(model, f)
+    f.close()
+print(if_Miriam(ramkowanie,delta_memory, path="waves/answer.wav"))
+print(if_Miriam(ramkowanie,delta_memory, path="miriam/1.wav"))
